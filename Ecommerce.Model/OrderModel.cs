c52140571556b2ed5a517e9fce1e86cc9f1c5002
jadhavpratik5433 <jadhavpratik5433@gmail.com>
2026-06-19 13:44:33 +0530

    gohead

---
+++ Ecommerce.Model/OrderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Model
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+
+    }
+}
---
+++ Ecommerce.ProductService/Data/ProductDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.ProductService.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options): base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductModel>().HasData(new ProductModel { Id = 1, Name = "Pratik", Quantity = 20, Price = 12 });
+            modelBuilder.Entity<ProductModel>().HasData(new ProductModel { Id = 2, Name = "sanket", Quantity = 50, Price = 12 });
+            modelBuilder.Entity<ProductModel>().HasData(new ProductModel { Id = 3, Name = "raj", Quantity = 30, Price = 12 });
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+}
